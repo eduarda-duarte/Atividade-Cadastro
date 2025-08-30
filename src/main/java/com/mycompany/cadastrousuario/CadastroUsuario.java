3443916268fbcf164e4b89a2ea8ad1d9518cc64a
eduarda-duarte <eduarda.duarte.unip@gmail.com>
2025-08-30 01:16:15 -0300

    first commit

---
+++ src/main/java/com/mycompany/cadastrousuario/CadastroUsuario.java
@@ -0,0 +1,16 @@
+package com.mycompany.cadastrousuario;
+/**
+ *
+ * @Eduarda Duarte
+ */
+public class CadastroUsuario {
+
+    public static void main(String[] args) {
+        
+        TelaCadastro janela = new TelaCadastro();
+        
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+       
+    }
+}
